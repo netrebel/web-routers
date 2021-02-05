9d7c9a59c8d930f42bcee873d2673edeb46a411b
Miguel Reyes <mreyes@life360.com>
2021-02-04 22:18:34 -0800

    handlerFunc to parse paths

--- main.go
+++ main.go
@@ -5,13 +5,20 @@ import (
 	"net/http"
 )
 
+func handlerFunc(rw http.ResponseWriter, r *http.Request) {
+	rw.Header().Set("content-type", "text/html")
+	fmt.Println(r.URL.Path)
+	if r.URL.Path == "/" {
+		fmt.Fprintf(rw, "<h1>Welcome</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprintf(rw, "Email me at hello@mail.com")
+	} else {
+		rw.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(rw, "Not Found")
+	}
+}
+
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		rw.Header().Set("content-type", "text/html")
-		fmt.Println(r.URL.Path)
-		if r.URL.Path == "/hello" {
-			fmt.Fprintf(rw, "<h1>Welcome</h1>")
-		}
-	})
+	http.HandleFunc("/", handlerFunc)
 	http.ListenAndServe(":8080", nil)
 }
