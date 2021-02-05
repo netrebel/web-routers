153cea157e99800d306691bf48934e74accba8b8
Miguel Reyes <mreyes@life360.com>
2021-02-04 22:37:27 -0800

    Custom NotFoundHandler

--- main.go
+++ main.go
@@ -17,8 +17,15 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Email me at hello@mail.com")
 }
 
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(w, "Sorry, not found!")
+}
+
 func main() {
 	mux := mux.NewRouter()
+	mux.NotFoundHandler = http.HandlerFunc(notFound)
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/contact", contact)
 	http.ListenAndServe(":8080", mux)
